aba292d43dd9803f8ca35eeb885c4feced6876e5
yun <indl1670@gmail.com>
2023-12-15 12:08:54 +0900

    💜[FEAT] 쇼핑몰 페이지 로그인 #LF1-697

--- mall/src/apis/auth.ts
+++ mall/src/apis/auth.ts
@@ -1,15 +1,18 @@
+import { loginDto } from "../recoil/common/interfaces";
 import { authInstance, defaultInstance } from "./utils";
 
 // 인가코드 전달
 export const kakaoLogin = async (code: string | null) => {
   const { data } = await defaultInstance.get(
-    "/auth/login/oauth2/code/kakao?code=" + code
+    "/auth/login/oauth2/kakao?code=" + code
   );
   return data;
 };
 
 // 로그인
-
+export const login = async (loginDto: loginDto) => {
+  return await defaultInstance.post("/auth/social/login", loginDto);
+};
 // 로그아웃
 export const logout = async (type: string) => {
   const { data } = await authInstance.post("/auth/social/" + type + "/logout");
--- mall/src/components/auth/KakaoLoginRedirect.tsx
+++ mall/src/components/auth/KakaoLoginRedirect.tsx
@@ -1,7 +1,7 @@
-import { useState, useEffect, useCallback } from "react";
+import { useEffect } from "react";
 import { useMutation } from "react-query";
 import { useNavigate } from "react-router";
-import { kakaoLogin } from "../../apis/auth";
+import { kakaoLogin, login } from "../../apis/auth";
 import { FailToast } from "../common/toast/FailToast";
 import { useSetRecoilState } from "recoil";
 import {
@@ -9,80 +9,39 @@ import {
   nicknameState,
   profileImageState,
 } from "../../recoil/atom/common";
-import { Modal, Button, Form, Input } from "antd";
+import { loginDto } from "../../recoil/common/interfaces";
 import MainPage from "../../pages/MainPage";
-import { registerPhoneNumber } from "../../apis/member";
-import { SuccessToast } from "../common/toast/SuccessToast";
 
 export default function KakaoLoginRedirect() {
   const url = new URL(window.location.href);
-  const error: string | null = url.searchParams.get("error");
-  const errorDescription: string | null =
-    url.searchParams.get("error_description");
   const code: string | null = url.searchParams.get("code");
 
   const navigate = useNavigate();
 
   const setIsLogin = useSetRecoilState<boolean>(loginState);
   const setNickname = useSetRecoilState<string>(nicknameState);
-  const setProfileImageState = useSetRecoilState<string>(profileImageState);
-  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
-  const [phoneNumber, setPhoneNumber] = useState<string>("");
+  const setProfileImage = useSetRecoilState<string>(profileImageState);
 
-  // 핸드폰번호 입력
-  const handlePhoneNumber = () => {
-    if (
-      phoneNumber !== "" &&
-      !isNaN(Number(phoneNumber)) &&
-      phoneNumber.length === 11
-    ) {
-      // 핸드폰번호 전송
-    }
-  };
-
-  const checkPhoneNumber = useCallback((_: any, value: string) => {
-    if (!value) {
-      return Promise.reject(new Error("핸드폰번호를 입력해주세요."));
-    }
-    if (isNaN(Number(value))) {
-      return Promise.reject(new Error("숫자만 입력해주세요."));
-    }
-    if (value.length < 11) {
-      return Promise.reject(new Error("정확한 핸드폰번호를 입력해주세요."));
-    }
-  }, []);
-
-  const handleSubmit = () => {
-    phoneNumberMutation.mutate();
-  };
-
-  const loginMutation = useMutation(["kakaoLogin"], () => kakaoLogin(code), {
+  const kakaoMutation = useMutation(["kakaoLogin"], () => kakaoLogin(code), {
     onSuccess: (data) => {
-      console.log(data);
-      // setIsLogin(true);
-      // setNickname(data.nickname);
-      // setProfileImageState(data.profileImage);
-
-      // if (data.phoneNumberIsRegistered) {
-      //   // 핸드폰번호 강제 입력
-      //   setIsModalOpen(true);
-      // }
-
-      // localStorage.setItem("accessToken", data["accessToken"]);
+      loginMutation.mutate(data);
     },
-    onError: (error: any) => {
-      console.log(error);
+    onError: () => {
       FailToast(null);
       navigate("/login");
     },
   });
 
-  const phoneNumberMutation = useMutation(
-    ["registerPhoneNumber"],
-    () => registerPhoneNumber(phoneNumber),
+  const loginMutation = useMutation(
+    ["login"],
+    (loginDto: loginDto) => login(loginDto),
     {
-      onSuccess: () => {
-        SuccessToast("저장되었습니다.");
+      onSuccess: (res) => {
+        setIsLogin(true);
+        setNickname(res.data.nickname);
+        setProfileImage(res.data.profileImage);
+        localStorage.setItem("accessToken", res.headers["authorization"]);
+        navigate("/");
       },
       onError: () => {
         FailToast(null);
@@ -91,63 +50,13 @@ export default function KakaoLoginRedirect() {
   );
 
   useEffect(() => {
-    if (!!error && !!errorDescription) {
-      alert(errorDescription);
-    } else {
-      loginMutation.mutate();
-    }
+    kakaoMutation.mutate();
     // eslint-disable-next-line
   }, []);
 
   return (
     <div>
-      <Modal
-        open={isModalOpen}
-        onCancel={() => setIsModalOpen(false)}
-        onOk={handlePhoneNumber}
-        title="개인정보 수집"
-        maskClosable={false}
-        footer={[
-          <Button
-            size="large"
-            type="primary"
-            style={{ width: "100%" }}
-            htmlType="submit"
-            onClick={handleSubmit}
-          >
-            저장
-          </Button>,
-        ]}
-      >
-        <div className="mt-4 mb-2 text-grayscale5 font-light">
-          원활한 사용을 위해 핸드폰 번호 입력이 필요합니다.
-        </div>
-        <Form
-          name="phoneNumberInputForm"
-          labelCol={{ span: 8 }}
-          onFinish={handlePhoneNumber}
-          initialValues={{ remember: true }}
-          autoComplete="off"
-        >
-          <Form.Item
-            name="phoneNumber"
-            rules={[
-              {
-                validator: checkPhoneNumber,
-              },
-            ]}
-          >
-            <Input
-              placeholder="핸드폰 번호 입력(- 없이 숫자만 입력해주세요)"
-              value={phoneNumber}
-              onChange={(e) => setPhoneNumber(e.target.value)}
-              maxLength={11}
-              minLength={11}
-              showCount
-            />
-          </Form.Item>
-        </Form>
-      </Modal>
+      <MainPage />
     </div>
   );
 }
