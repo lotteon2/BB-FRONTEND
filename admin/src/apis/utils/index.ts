82c8c71e97938059ea8b1756367e99f1fc47b027
yun <indl1670@gmail.com>
2023-12-15 13:18:19 +0900

    💜[FIX] 관리자 페이지 instance 수정

--- admin/src/apis/utils/index.ts
+++ admin/src/apis/utils/index.ts
@@ -5,7 +5,7 @@ const BASE_URL = process.env.REACT_APP_API_URL;
 const axiosApi = (baseURL: string | undefined) => {
   const instance = axios.create({
     baseURL,
-    withCredentials: true,
+    withCredentials: false,
   });
   return instance;
 };
@@ -15,6 +15,20 @@ const axiosAuthApi = (baseURL: string | undefined) => {
     baseURL,
     withCredentials: true,
   });
+
+  instance.interceptors.request.use(
+    (config) => {
+      const access_token = localStorage.getItem("accessToken");
+      if (access_token) {
+        config.headers.Authorization = access_token;
+      }
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+
   return instance;
 };
 
