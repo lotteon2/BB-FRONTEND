53eaffb08c93a58829b9e95789c4bf22fd8fd9b7
yun <indl1670@gmail.com>
2024-01-18 10:14:53 +0900

    💜[TEST]

--- mall/src/apis/utils/index.ts
+++ mall/src/apis/utils/index.ts
@@ -5,7 +5,7 @@ const BASE_URL = process.env.REACT_APP_API_URL;
 const axiosApi = (baseURL: string | undefined) => {
   const instance = axios.create({
     baseURL,
-    withCredentials: true,
+    withCredentials: false,
   });
   return instance;
 };
@@ -13,7 +13,7 @@ const axiosApi = (baseURL: string | undefined) => {
 const axiosAuthApi = (baseURL: string | undefined) => {
   const instance = axios.create({
     baseURL,
-    withCredentials: true,
+    withCredentials: false,
   });
 
   instance.interceptors.request.use(
